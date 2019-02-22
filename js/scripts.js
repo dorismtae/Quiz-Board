79683d22b5b467da2c56790c5686eaf1b7dc207a
dorismtae <dorismtae@gmail.com>
2019-02-22 17:51:56 +0300

    added changes in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-
+$(document).ready(function(){
+  $("#question-board").submit (function(){
+  var qOne = $("input:radio[name=q1]:checked").val();
+  var qTwo = $("input:radio[name=q2]:checked").val();
+  var qThree = $("input:radio[name=q3]:checked").val();
+  var qFour = $("input:radio[name=q4]:checked").val();
+  var qFive = $("input:radio[name=q5]:checked").val();
+  var Result = 0;
+  if (qOne ==="Document Object model"){
+    Result += 20;
+  }
+  else {
+    Result +=0;
+  }
+  if (qTwo ==="Yes"){
+    Result += 20;
+  }
+  else {
+    Result +=0;
+  }
+  if (qThree==="Error in code"){
+    Result += 20;
+  }
+  else {
+    Result +=0;
+  }
+  if (qFour ==="Inline element"){
+    Result += 20;
+  }
+  else {
+    Result +=0;
+  }
+  if (qFive ==="remainder"){
+    Result += 20;
+  }
+  else {
+    Result +=0;
+  }
+alert(Result);
+});
+});
